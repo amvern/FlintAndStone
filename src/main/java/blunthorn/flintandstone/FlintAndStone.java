0c6f4af79d0ab0a5de23e5255f7493a85cc222b2
amvern <drkmannn@gmail.com>
2025-12-17 01:54:48 -0800

    1.21.1

--- src/main/java/blunthorn/flintandstone/FlintAndStone.java
+++ src/main/java/blunthorn/flintandstone/FlintAndStone.java
@@ -8,8 +8,6 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
@@ -20,23 +18,19 @@ public class FlintAndStone implements ModInitializer {
 	public static final String MOD_ID = "flintandstone";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public final Item ITEM = getRegisteredFlintAndStone();
-
-	private Item getRegisteredFlintAndStone(){
-		Item.Settings fabricItemSettings = new Item.Settings()
-			.maxDamage(1);
-
-		final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "flint_and_stone"));
-		return Items.register(registryKey, FlintAndStoneItem::new, fabricItemSettings);
-	}
+	public static final Item FLINT_AND_STONE = Registry.register(
+			Registries.ITEM,
+            Identifier.of(MOD_ID, "flint_and_stone"),
+			new FlintAndStoneItem(new Item.Settings().maxDamage(1))
+	);
 
 	private void registerFlintAndStoneToToolItemGroup(FabricItemGroupEntries entries){
-		entries.addBefore(Items.FLINT_AND_STEEL,ITEM);
+		entries.addBefore(Items.FLINT_AND_STEEL, FLINT_AND_STONE);
 	}
 
 	@Override
 	public void onInitialize() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(this::registerFlintAndStoneToToolItemGroup);
-		LOGGER.info(MOD_ID + " has loaded item: " + ITEM.toString());
+		LOGGER.info(MOD_ID + " has loaded item: " + FLINT_AND_STONE);
 	}
 }
